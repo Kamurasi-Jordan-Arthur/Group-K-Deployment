5c10b819499be89899f5685a8b8aae5b5bdaed88
Jordan <kamurasijordanarthur@gmail.com>
2023-05-09 15:22:31 +0300

    last hope deploy

--- LibApp/settings.py
+++ LibApp/settings.py
@@ -47,7 +47,7 @@ ALLOWED_HOSTS = [
                  'https://cloudlibrary-3e55dc50-a051-4d41-b8df.cranecloud.io',
                 #  '127.0.0.1', 
                  'https://group-k-deployment-production.up.railway.app',
-                #  'https://libapp-99a6f10f-b1ab-4adc-a1df.cranecloud.io/',
+                #  'https://libapp-99a6f10f-b1ab-4adc-a1df.cranecloud.io',
                 #  'https://libapp-74269821-ecf4-4b95-afdc.cranecloud.io',
                  '*',
                  ]
